38eee6444ba4a095151a05699f5c6a631a8d72af
Vietcuong0508 <cuongtvth2009009@fpt.edu.vn>
2022-10-27 11:15:25 +0700

    duc ngu

---
+++ src/main/java/com/example/forums_backend/seed/SeederPreparation.java
@@ -0,0 +1,41 @@
+package com.example.forums_backend.seed;
+
+import com.example.forums_backend.seed.feature.SeederHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+/**
+ * <div>
+ *     Chỉ chạy trong môi trường test, không được phép sử dụng trong môi trường production.
+ *     Class chỉ dùng trong môi trường test, dùng để load dữ liệu seed cho một test case. Hàm run được chạy mỗi khi project bắt đầu.
+ *     Dùng để đọc dữ liệu từ file excel và insert vào database. File excel được đặt trong thư mục resources/seed của project spring.
+ *     Trong file dữ liệu được chia thành các hàng và cột, cột là các trường trong database tương ứng.
+ *     Tên file được truyền vào là một tham số khi chạy project, ví dụ:
+ * </div>
+ * <code>
+ *     ./mvnw spring-boot:run -Dspring-boot.run.arguments="file_01.xlsx  file_02.xlsx file_03.xlsx"
+ * </code>
+ * <p>
+ *     Có thể chạy và load dữ liệu từ nhiều file, các file cách nhau bởi dấu cách.
+ * </p>
+ */
+//@Profile("test")
+@Component
+public class SeederPreparation implements CommandLineRunner {
+    /**
+     * Chuyển qualifier theo tên @Component mới tạo.
+     * */
+    @Autowired
+    @Qualifier("jsonSeeder")
+    SeederHelper seederHelper;
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Prepare seed data...");
+        seederHelper.readManuallyFromResources();
+        System.out.println("Action success!");
+    }
+}
---
+++ src/main/java/com/example/forums_backend/seed/feature/JsonSeederHelper.java
@@ -0,0 +1,114 @@
+package com.example.forums_backend.seed.feature;
+
+import com.example.forums_backend.entity.Account;
+import com.example.forums_backend.security.config.repository.AccountRepository;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializer;
+import lombok.RequiredArgsConstructor;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Đọc nội dung file excel, parse kiểu json để tránh việc ép kiểu nhiều trường, giảm việc cho dev.
+ */
+//@Profile("dev")
+@Component("jsonSeeder")
+@RequiredArgsConstructor
+public class JsonSeederHelper implements SeederHelper {
+
+    final AccountRepository accountRepository;
+    private static final String DEFAULT_PATH = "classpath:seed/";
+    // Tên file dữ liệu được lấy từ tester và lưu trong thư mục resources/seed
+    private static final String FILE_NAME = "account_seed.xlsx";
+
+    private static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    private static final Gson gson = new GsonBuilder().registerTypeAdapter(LocalDateTime.class, (JsonDeserializer<LocalDateTime>) (json, typeOfT, context) -> LocalDateTime.parse(json.getAsString(), DateTimeFormatter.ofPattern(DATE_TIME_FORMAT))).create();
+
+    @Override
+    public void readManuallyFromResources() throws IOException {
+        File file = ResourceUtils.getFile(DEFAULT_PATH + FILE_NAME);
+        FileInputStream fileInputStream = new FileInputStream(file);
+        Workbook workbook = new XSSFWorkbook(fileInputStream);
+        System.out.println("Feel like reading file is ok.");
+        // Có thể đọc nhiều sheet và save vào nhiều bảng.
+        // Đọc sheet đầu tiên.
+        Sheet sheet = workbook.getSheetAt(0);
+        // Tạo danh sách đối tượng để lưu.
+        List<Account> accounts = new ArrayList<>();
+        boolean hasData = true;
+        boolean isColumnName = true;
+        // Duyệt từng dòng trong sheet.
+        List<String> keys = new ArrayList<>();
+        for (Row row : sheet) {
+            // xử lý lấy danh mục các trường
+            if (isColumnName) {
+                for (Cell cell : row) {
+                    keys.add(cell.getStringCellValue());
+                }
+                isColumnName = false;
+                continue;
+            }
+            // Mỗi dòng là một đối tượng cần lưu, tạo mới đối tượng.
+            Account obj;
+            // Duyệt từng cột để lấy dữ liệu đưa vào đối tượng.
+            HashMap<String, Object> jsonMap = new HashMap<>();
+            Object idValue = null;
+            for (Cell cell : row) {
+                Object objValue = null;
+                switch (cell.getCellType()) {
+                    case NUMERIC:
+                        objValue = cell.getNumericCellValue();
+                        break;
+                    case BOOLEAN:
+                        objValue = cell.getBooleanCellValue();
+                        break;
+                    case _NONE:
+                    case BLANK:
+                    case ERROR:
+                        break;
+                    default:
+                        objValue = cell.getStringCellValue();
+                        break;
+                }
+                if("id".equals(keys.get(cell.getColumnIndex()))) {
+                    idValue = objValue;
+                }
+                if (idValue == null) {
+                    hasData = false;
+                    break;
+                }
+                jsonMap.put(keys.get(cell.getColumnIndex()), objValue);
+            }
+            if (!hasData) {
+                break;
+            }
+            String jsonData = gson.toJson(jsonMap);
+            System.out.println(jsonData);
+            try {
+                obj = gson.fromJson(jsonData, Account.class);
+                accounts.add(obj);
+            } catch (Exception ex) {
+                System.err.println("Can't parse information from json.\nError: " + ex.getMessage());
+            }
+        }
+//        System.out.println(gson.toJson(products));
+        workbook.close();
+        // Lưu thông tin vào database.
+        accountRepository.saveAll(accounts);
+    }
+}
