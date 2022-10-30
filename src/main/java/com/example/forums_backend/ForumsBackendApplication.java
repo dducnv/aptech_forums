63c3ce9aed314c809f8c34a5a2a07331724c85e0
dducnv0712 <ducbe2k2@gmail.com>
2022-10-30 16:57:04 +0700

    Time zone set

--- src/main/java/com/example/forums_backend/ForumsBackendApplication.java
+++ src/main/java/com/example/forums_backend/ForumsBackendApplication.java
@@ -5,10 +5,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableConfigurationProperties(AppProperties.class)
 public class ForumsBackendApplication {
-
+    @PostConstruct
+    public void init(){
+        // Setting Spring Boot SetTimeZone
+        TimeZone.setDefault(TimeZone.getTimeZone("ICT"));
+    }
     public static void main(String[] args) {
         SpringApplication.run(ForumsBackendApplication.class, args);
     }
